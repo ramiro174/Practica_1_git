8e1c5f1b4f49e0493df8833107bf40c34ca6b608
ramiro174 <ramiro174@gmail.com>
2025-09-11 13:58:21 -0600

    cambiio prueba

--- app/src/main/java/com/example/practica_1_git/MainActivity.kt
+++ app/src/main/java/com/example/practica_1_git/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -34,6 +35,12 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
 
+    Box(){
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
